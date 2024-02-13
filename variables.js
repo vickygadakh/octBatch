9e390ab914a3d17b110efa12bc2e3340b3263b8c
TheBeastVicky <vikigadakh2019@gmail.com>
2024-02-13 23:17:03 +0530

    intro to js and variables

---
+++ variables.js
@@ -0,0 +1,21 @@
+//Variables are Containers for Storing Data
+//JavaScript Variables can be declared in 4 ways:
+
+//Automatically
+//Using var
+//Using let
+//Using const
+
+// let/var/const vN = value
+
+const marks = 50;
+console.log(marks);
+
+marks1=90;
+
+let name ; //declaration
+console.log(name);
+name = "vicky" ; //defination
+console.log(name);
+
+let sName = "gadakh" //declaration + defination
