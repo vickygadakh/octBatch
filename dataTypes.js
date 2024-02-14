b15e6b26546880e62d114f727322b40991b078e5
TheBeastVicky <vikigadakh2019@gmail.com>
2024-02-14 11:17:50 +0530

    dt

---
+++ dataTypes.js
@@ -0,0 +1,24 @@
+// data types: 1.premitive data types 
+//             2.non-premitive data types
+
+// 1.premitive data types:single value holder-- in javascript include
+// 1.Number
+// 2.String
+// 3.Boolean
+// 4.Undefined
+// 5.Null
+// 6.Object
+// 7.Symbol
+
+// 2.Non-premative: object
+
+//number : whole no or decimal no
+let amount = 98.9898; //76767777
+
+//typeof(): is used to get data types of data
+
+console.log(typesof(amount));
+
+//string dt ; ''/""
+let city = 'Mumbai is big city';
+console.log(typesof(city));
