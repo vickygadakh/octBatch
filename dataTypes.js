7716a867133be3b4c45ae459f1d217944626e6c2
TheBeastVicky <vikigadakh2019@gmail.com>
2024-02-18 23:33:42 +0530

    datatypes

--- dataTypes.js
+++ dataTypes.js
@@ -17,8 +17,75 @@ let amount = 98.9898; //76767777
 
 //typeof(): is used to get data types of data
 
-console.log(typesof(amount));
+console.log(typeof(amount));
 
 //string dt ; ''/""
-let city = 'Mumbai is big city';
-console.log(typesof(city));
+let city = 'Mumbai';
+console.log(typeof(city));
+
+//Boolean:true/false
+let isFavMovie = true;//"true" //string
+console.log(typeof(isFavMovie));
+
+//undefined
+let a ;
+console.log(a);
+console.log(typeof(a));
+
+let b = undefined;
+console.log(b);
+console.log(typeof(b));
+
+//var false = "vicky" //we can not declare reserved keyword as a variable name
+
+let r = "none" //string
+
+let c = null;
+console.log('c',typeof(c));
+document.write(c,'<br>')
+
+let cash = BigInt("9999999999999999"); //1.3656 //1.37
+document.write(cash,'<br>')
+
+var stdName = "vicky,om,rinku"
+
+//array:[]: listing >>heterogenous array
+let colors=['red','orange','black'];//homogenous array
+var studentNames = ["pravin",400,true,"om","sidh","pranav",test()]//heterogenous array
+document.write("colors >>",colors)
+document.write("studentNames >> ",studentNames)
+console.log(studentNames);
+
+function test(){
+return 50*20;
+}
+
+var studentNames = ["pravin",400,true,"om","sidh","pranav",test()]//heterogenous arr
+
+let colors2 = ['red','orange','black',"yellow"];
+
+document.write("<br>",colors2[6])
+
+let cars = []; //empty array
+console.log("cars>>",cars);
+
+//length operator : it gives length of array or string
+
+console.log(colors2.length);
+
+//object: {key : value}
+let std = { name:"vickyyy" ,age:25,city:"pune"};
+let emp = {}; //empty obj
+console.log(std.city);
+
+//array of obj
+let std2= [
+    { name:"vickyyy" ,age:25,city:"pune"},
+    { name:"rushi" ,age:30,city:"mumbai"},
+    { name:"pravin" ,age:28,city:"satara"}
+]
+
+console.log(std2[0]);
+
+console.log(std2[0].age);
+console.log(std2.length);
