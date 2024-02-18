f09c846b64cc41d7452ebb2c46eeb26edf05833a
TheBeastVicky <vikigadakh2019@gmail.com>
2024-02-18 23:57:58 +0530

    string m

---
+++ stringmethods.js
@@ -0,0 +1,9 @@
+let text = "vicky hjhbhjh   hhhh";
+console.log(text.length);
+
+let text1="i like mango";
+console.log(text1.slice(1,6));//start ind:including end ind:exclusive
+
+console.log(text1.slice(1));
+
+console.log(text1.slice(-6,-1));//-6 is exclusive, -1 inclusive
