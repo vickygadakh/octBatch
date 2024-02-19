b84812e7612d0b0bf86b44d80d8dcddd1e8fc6ce
TheBeastVicky <vikigadakh2019@gmail.com>
2024-02-19 23:42:16 +0530

    sm

--- stringmethods.js
+++ stringmethods.js
@@ -7,3 +7,29 @@ console.log(text1.slice(1,6));//start ind:including end ind:exclusive
 console.log(text1.slice(1));
 
 console.log(text1.slice(-6,-1));//-6 is exclusive, -1 inclusive
+
+console.log(text1.substring(-5,9));//neg index will treated as 0 here
+console.log(text1.substr(4,6));//end index will consider here as a length
+
+//The replace() method does not change the string it is called on.
+
+//The replace() method returns a new string.
+
+//The replace() method replaces only the first match
+
+//If you want to replace all matches, use a regular expression with the /g flag set. Se
+
+var details = "mumbai is metrocity. i like mumbai";
+console.log(details.replace("mumbai","pune"));
+console.log(details.replace(/mumbai/g,"pune"));
+console.log(details.replace(/mumbai/i,"pune"));
+console.log(details.replace(/mumbai/ig,"pune"));
+
+//replaceAll
+console.log(details.replaceAll("mumbai","pppppp"));
+console.log(details.replaceAll(/mumbai/ig,"pppppp"));
+
+console.log(details.toUpperCase());
+console.log(details.toLowerCase());
+
+
