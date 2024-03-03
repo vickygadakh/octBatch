07ac999d5eaddb8850849539521791c4192175b9
TheBeastVicky <vikigadakh2019@gmail.com>
2024-03-03 20:06:42 +0530

    eyeicon

--- angular/myApplication/src/app/user/user-login/user-login.component.ts
+++ angular/myApplication/src/app/user/user-login/user-login.component.ts
@@ -7,12 +7,16 @@ import { Component } from '@angular/core';
 })
 export class UserLoginComponent {
 
-name="Vicky"
+name="Vicky";
+isShowPass = false;
   //template driven : form fields are less
   //reactive form : form fields are more, need custom validation
 
   login(data:any){
    console.log(data);
    
+  }
+  toShowPassword(){
+   this.isShowPass = ! this.isShowPass;
   }
 }
